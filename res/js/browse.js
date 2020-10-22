75629ea04de5c254724dcfd40ba84a8d73350e4f
karl-martin-karno <56890340+karl-martin-karno@users.noreply.github.com>
2020-10-22 12:54:01 +0300

    Button fix.

--- res/js/browse.js
+++ res/js/browse.js
@@ -1,44 +1,41 @@
-$ (function(){
-    $.get('https://private-anon-55b9e17f14-wad20postit.apiary-mock.com/profiles', function (response){
-        for (post of response){
+function buttonClicked() {
+    $('.follow').click(function () {
+        var $this = $(this);
+        if ($this.hasClass('follow')) {
+            $this.removeClass("follow").addClass("followed")
+            $this.text('Followed');
+        } else {
+            $this.removeClass("followed").addClass("follow")
+            $this.text('Follow');
+        }
+    });
+}
+
+$(function () {
+    $.get('https://private-anon-55b9e17f14-wad20postit.apiary-mock.com/profiles', function (response) {
+
+        for (post of response) {
             let div = $('<div class="profile-container">')
-            $('.follow').click(function(){
-                var $this = $(this);
-                $this.removeClass("followed")
-                $this.toggleClass('follow');
-                if($this.hasClass('follow')){
 
-                    $this.text('Follow');
-                } else {
-                    $this.removeClass("follow").addClass("followed")
-                    $this.text('Followed');
-                }
-
-            });
-            let img = $('<img class="profile-avatar">').attr('src',post.avatar)
+            let img = $('<img class="profile-avatar">').attr('src', post.avatar)
             let firstname = $('<h1>').text(post.firstname)
             let lasttname = $('<h1>').text(post.lastname)
-            let button = $('<button class="follow">').text("Follow")
 
+            buttonClicked()
+
+            let button = $('<button class="follow">').text("Follow")
             div.append(img)
             div.append(firstname)
             div.append(lasttname)
             div.append(button)
-
             $('section').append(div);
+
+            buttonClicked()
+
+
         }
-
-
     })
-    $('.follow').click(function(){
-        var $this = $(this);
-        $this.toggleClass('follow');
-        if($this.hasClass('follow')){
-            $this.text('Follow');
-        } else {
-            $this.text('Followed');
-        }
-    });
-
 
 });
+
+
