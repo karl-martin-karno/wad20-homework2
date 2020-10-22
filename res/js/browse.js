c8d93c98c564e2a130f55db717d96ab1b6910638
karl-martin-karno <56890340+karl-martin-karno@users.noreply.github.com>
2020-10-22 13:37:10 +0300

    layout fixed.

--- res/js/browse.js
+++ res/js/browse.js
@@ -15,19 +15,20 @@ $(function () {
     $.get('https://private-anon-55b9e17f14-wad20postit.apiary-mock.com/profiles', function (response) {
 
         for (post of response) {
-            let div = $('<div class="profile-container">')
 
+            let div = $('<div class="profile-container">')
+            let div2 = $('<div class="profile-container-item">')
             let img = $('<img class="profile-avatar">').attr('src', post.avatar)
-            let firstname = $('<h1>').text(post.firstname)
-            let lasttname = $('<h1>').text(post.lastname)
+            let fullname = $('<h1>').text(post.firstname).append(" "+ post.lastname)
+
 
             buttonClicked()
 
             let button = $('<button class="follow">').text("Follow")
-            div.append(img)
-            div.append(firstname)
-            div.append(lasttname)
-            div.append(button)
+            div2.append(img)
+            div2.append(fullname)
+            div2.append(button)
+            div.append(div2);
             $('section').append(div);
 
             buttonClicked()
